17ae15fdf01605853d2386d119a93048026ceb52
rmv4714 <shwetakamal4714@gmail.com>
2023-10-25 12:48:41 +0530

    post-form

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 
-export { Header, Footer, Logo, Container, LogoutBtn };
+export { Header, Footer, Logo, Container, LogoutBtn, RTE };
